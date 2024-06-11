aa980f96ee7cd8562528286684f8d8177c245f4b
Sergiu Popistas <sergiu.popistas@itschool.ro>
2024-06-11 20:38:35 +0300

    implementare binary search

--- AlgoritmiCautare.cpp
+++ AlgoritmiCautare.cpp
@@ -14,12 +14,39 @@ int linear_search(std::vector<int> input,int number)
     return -1;
 }
 
+int binarySearch(std::vector<int> input, int l, int r, int x)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+        //verificam daca elemntul cautat se afla la mijloc
+        if (input[mid] == x)
+        {
+            return mid;
+        }
+        //daca elementul cautat este mai mic
+        // chemam recursiv functia cu idexi de la stanga pana la mijloc (paretea stanga)
+        if (input[mid] > x)
+            return binarySearch(input, l, mid - 1, x);
+
+        //in caz contrar se afla in partea dreapta ( mai mare)
+        else
+        {
+            return binarySearch(input, mid + 1, r, x);
+        }
+    }
+    //daca am ajuns pana aici inseamna ca elementul nu exista
+    return -1;
+}
+
+
 
 int main()
 {
-    std::vector<int> data{1, 4, 5, 0, -1, 17};
+    std::vector<int> data{1, 2, 5, 44, 57, 99};
 
-    int cautare = 45;
-    std::cout << "Elementul " << cautare << " se afla la indexul " << linear_search(data, cautare);
+    int cautare = 99;
+    //std::cout << "Elementul " << cautare << " se afla la indexul " << linear_search(data, cautare);
+    std::cout << "Elementul " << cautare << " se afla la indexul " << binarySearch(data, 0, data.size(), cautare);
 
 }
